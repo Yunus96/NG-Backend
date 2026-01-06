027244d1c75ce5acd46ef64c29b9d5e3c2bf3376
Yunus96 <mohammadyunus102@gmail.com>
2026-01-06 12:29:21 +0530

    BE4_AS1 complete

---
+++ db/db.connect.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+require("dotenv").config();
+// db.connect.js
+
+const mongoUri = process.env.mongoUri;
+
+const initializeDatabase = async () => {
+    await mongoose
+            .connect(mongoUri).
+            then(() => {
+                console.log("Connected to Database");
+            })
+            .catch((error) => console.log("Error connecting to Database", error));
+        }
+
+module.exports = { initializeDatabase };
+
+// Initialize the database connection
+//initializeDatabase();
--- index.js
+++ index.js
@@ -2,9 +2,140 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 
+const { initializeDatabase }   = require('./db/db.connect.js');
+
+const Book = require("./models/book.models.js");
+    console.log(Book)
+
 app.use(cors());
 app.use(express.json());
 
+initializeDatabase();
+//Q1 of BE4_Assignment1
+app.post("/books", async (req, res) => {
+
+  try {
+    const book = new Book(req.body);
+    const savedBook = await book.save();
+    res.status(201).json(savedBook);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+});
+
+//Q3 of BE4_Assignment1
+app.get("/books", async (req, res) => {
+  try {
+    const books = await Book.find();
+    res.json(books);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+//Q4 of BE4_Assignment1
+app.get("/books/title/:title", async (req, res) => {
+  try {
+    const book = await Book.findOne({ title: req.params.title });
+    if (!book) {
+      return res.status(404).json({ message: "Book does not exist" });
+    }
+    res.json(book);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+//Q5 of BE4_Assignment1
+app.get("/books/author/:author", async (req, res) => {
+  try {
+    const books = await Book.find({ author: req.params.author });
+    res.json(books);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+//Q6 of BE4_Assignment1
+app.get("/books/genre/:genre", async (req, res) => {
+  const genre = req.params.genre
+  try {
+    const books = await Book.find({ genre: genre });
+    res.json(books);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+//Q7 of BE4_Assignment1
+app.get("/books/year/:year", async (req, res) => {
+  const year = parseInt(req.params.year);
+  try {
+    const books = await Book.find({ publishedYear: year });
+    res.json(books);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+//Q8 of BE4_Assignment1
+app.post("/books/:id/rating", async (req, res) => {
+  try {
+    const book = await Book.findByIdAndUpdate(
+      req.params.id,
+      { rating: req.body.rating },
+      { new: true }
+    );
+
+    if (!book) {
+      return res.status(404).json({ message: "Book does not exist" });
+    }
+
+    res.json(book);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+});
+
+//Q9 of BE4_Assignment1
+app.post("/books/update/:title", async (req, res) => {
+  try {
+    const book = await Book.findOneAndUpdate(
+      { title: req.params.title },
+      req.body,
+      { new: true }
+    );
+
+    if (!book) {
+      return res.status(404).json({ message: "Book does not exist" });
+    }
+
+    res.json(book);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+});
+
+//Q10 of BE4_Assignment1
+app.delete("/books/:id", async (req, res) => {
+  try {
+    const book = await Book.findByIdAndDelete(req.params.id);
+    if (!book) {
+      return res.status(404).json({ message: "Book not found" });
+    }
+    res.json({ message: "Book deleted successfully" });
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+});
+
+
+
+
+
+
+
+//Vercel Deployement Assignment
 const users = [
   {
     id: 1,
